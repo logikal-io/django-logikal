e4e6a6e3ae13eca6b0e482d8d6ced7650547f83e
CristinaNeacsu <cristina.neacsu@logikal.io>
2026-04-30 18:33:53 +0200

    Clean up and fix layout

--- django_logikal/static/django_logikal/js/commons.js
+++ django_logikal/static/django_logikal/js/commons.js
@@ -1,10 +1,8 @@
 const menuItem = document.querySelectorAll('menu li');
 
 menuItem.forEach(item => {
-  console.log(item)
   item.addEventListener('click', function () {
     const subMenu = this.querySelector('ul');
-    console.log(subMenu)
     if (subMenu) {
       const isOpen = subMenu.classList.toggle('open');
       if (isOpen) {
