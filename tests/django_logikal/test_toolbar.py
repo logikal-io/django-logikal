fdb73ce54a05fcba18c41eb643d349942af42150
GergelyKalmar <gergely.kalmar@logikal.io>
2026-08-21 22:48:25 +0200

    Update debug toolbar

--- tests/django_logikal/test_toolbar.py
+++ tests/django_logikal/test_toolbar.py
@@ -9,20 +9,21 @@ def test_toolbar(live_app_url_with_toolbar: LiveURL, browser: Browser) -> None:
     # Load page
     browser.get(live_app_url_with_toolbar('models'))
     browser.check('handle')
-    browser.find_element(By.ID, 'djDebugToolbarHandle').click()
+    toolbar = browser.find_element(By.ID, 'djDebugRoot').shadow_root
+    toolbar.find_element(By.ID, 'djDebugToolbarHandle').click()
 
     # Freeze undeterministic values
-    timer_panel = browser.find_element(By.ID, 'djdt-TimerPanel')
+    timer_panel = toolbar.find_element(By.ID, 'djdt-TimerPanel')
     browser.replace_text(timer_panel.find_element(By.TAG_NAME, 'small'), 'CPU: 0.00ms (0.00ms)')
 
-    template_panel = browser.find_element(By.ID, 'djdt-TemplateProfilerPanel')
+    template_panel = toolbar.find_element(By.ID, 'djdt-TemplateProfilerPanel')
     browser.replace_text(template_panel.find_element(By.TAG_NAME, 'small'), '1 calls in 0.00 ms')
 
-    sql_panel = browser.find_element(By.ID, 'djdt-SQLPanel')
+    sql_panel = toolbar.find_element(By.ID, 'djdt-SQLPanel')
     browser.replace_text(sql_panel.find_element(By.TAG_NAME, 'small'), '1 query in 0.00ms')
 
     # Check toolbar
     browser.check('menu')
-    browser.find_element(By.ID, 'djdt-CachePanel').click()
-    browser.find_element(By.ID, 'CachePanel').find_element(By.TAG_NAME, 'h4')  # waits for loading
+    toolbar.find_element(By.ID, 'djdt-CachePanel').click()
+    toolbar.find_element(By.ID, 'CachePanel').find_element(By.TAG_NAME, 'h4')  # waits for loading
     browser.check('page')
