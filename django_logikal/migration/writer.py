b77179f722b5b5d9213a7d19663bbb8b7732608f
GergelyKalmar <gergely.kalmar@logikal.io>
2026-08-30 23:38:43 +0200

    Update dependencies

--- django_logikal/migration/writer.py
+++ django_logikal/migration/writer.py
@@ -25,4 +25,6 @@ class FormattedMigrationWriter(MigrationWriter):
         max_line_length = get_ini_option('max_line_length')
         code = super().as_string()
         code = black.format_str(code, mode=black_mode(max_line_length))
-        return isort.api.sort_code_string(code, **isort_config(max_line_length=max_line_length))
+        return isort.api.sort_code_string(  # pylint: disable=c-extension-no-member
+            code, **isort_config(max_line_length=max_line_length),
+        )
